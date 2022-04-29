e957d63cb83b31d3fffa3b53c0c6ff44051bd901
Ethan C <36670680+etbikehome@users.noreply.github.com>
2022-04-29 13:11:14 -0400

    Updated Scan and Plot

--- Plot_Map.m
+++ Plot_Map.m
@@ -9,10 +9,16 @@ map1 = plot_Lidar(ralldata(:,1), thetaalldata(:,1), 0, 0, 0);
 map2 = plot_Lidar(ralldata(:,2), thetaalldata(:,2), - pi/2, 0, 0);
 map3 = plot_Lidar(ralldata(:,3), thetaalldata(:,3), 0, 0, -2);
 map4 = plot_Lidar(ralldata(:,4), thetaalldata(:,4), pi, -1.5, -1);
+map5 = plot_Lidar(ralldata(:,4), thetaalldata(:,4), pi, 2, -2.5);
+map6 = plot_Lidar(ralldata(:,4), thetaalldata(:,4), -pi/2, 1.5, .5);
 
 %display on one plot
-scatter(map1(1,:), map1(2,:));
 hold on
+scatter(map1(1,:), map1(2,:));
 scatter(map2(1,:), map2(2,:));
 scatter(map3(1,:), map3(2,:));
 scatter(map4(1,:), map4(2,:));
+scatter(map5(1,:), map5(2,:));
+scatter(map6(1,:), map6(2,:));
+hold off 
+axis equal
--- collectScans.m
+++ collectScans.m
@@ -42,7 +42,27 @@ scan_message = receive(sub);
 r_4 = scan_message.Ranges(1:end-1);
 theta_4 = deg2rad([0:359]');
 
+% place Neato at an arbitrary position and orientation
+placeNeato(2,-2.5,-1,0)
+
+% wait a while for the Neato to fall into place
+pause(2);
+
+scan_message = receive(sub);
+r_5 = scan_message.Ranges(1:end-1);
+theta_5 = deg2rad([0:359]');
+
+% place Neato at an arbitrary position and orientation
+placeNeato(1.5,.5, 0, -1)
+
+% wait a while for the Neato to fall into place
+pause(2);
+
+scan_message = receive(sub);
+r_6 = scan_message.Ranges(1:end-1);
+theta_6 = deg2rad([0:359]');
+
 % Shove everything into a matrix (you can use the matrix or the
 % individual r_x and theta_x variables
-r_all = [r_1 r_2 r_3 r_4];
-theta_all = [theta_1 theta_2 theta_3 theta_4];
+r_all = [r_1 r_2 r_3 r_4 r_5 r_6];
+theta_all = [theta_1 theta_2 theta_3 theta_4 theta_5 theta_6];
