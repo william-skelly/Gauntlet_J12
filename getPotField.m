cb52a47b9ad280028ae6848ee4c082dcf3ab6b76
wskelly <wskelly@DMBQ2F3.olin.edu>
2022-05-09 15:35:36 -0400

    Added mid-point distance factor

--- getPotField.m
+++ getPotField.m
@@ -11,10 +11,10 @@ function res = getPotField()
     %%%%%%%%%%%%%%%
     
     %domain limit coordinates (m)
-    x_min = -1.5;
-    x_max = 2.5;
-    y_min = -3.37;
-    y_max = 1;
+    x_min = -2;%-1.5;
+    x_max = 3;%2.5;
+    y_min = -4;%-3.37;
+    y_max = 1.5;%1;
     
     %create meshgrid
     boundry_step = 0.01; %domain step size (m)
@@ -31,30 +31,54 @@ function res = getPotField()
     left_wall   = pointsFromLine([1;0], [-1.5,-3.37], [2.5,-3.37]);
     right_wall  = pointsFromLine([1;0], [-1.5,1], [2.5,1]);
     
-    all_sources = [top_wall; bottom_wall; left_wall; right_wall];
-    
-    
     %%%%%%%%%%%%%%%%
     % Create Sources
     %%%%%%%%%%%%%%%%
     
     z_field = zeros(size(x_space, 1), size(x_space, 2)); %initialize z_field
     
-    %loop through all source points and create sources there
-    for index = 1:length(all_sources)
-        x_source = all_sources(index, 1);
-        y_source = all_sources(index, 2);
-        new_source = -1 * make_sink(x_source, y_source, x_space, y_space) * 1/(length(top_wall));
-        z_field(1,1)
+    %top wall
+    for index = 1:length(top_wall)
+        x_source = top_wall(index, 1);
+        y_source = top_wall(index, 2);
+        distToMid = sqrt((x_source + 1.5).^2 + (y_source + 1).^2);
+        new_source = -1 * make_sink(x_source, y_source, x_space, y_space) * distToMid/(length(top_wall));
         z_field = z_field + new_source;
     end
-   
+    
+    %bottom wall
+    for index = 1:length(bottom_wall)
+        x_source = bottom_wall(index, 1);
+        y_source = bottom_wall(index, 2);
+        distToMid = sqrt((x_source - 2.5).^2 + (y_source + 1).^2);
+        new_source = -1 * make_sink(x_source, y_source, x_space, y_space) * distToMid/(length(bottom_wall));
+        z_field = z_field + new_source;
+    end
+    
+    %left wall
+    for index = 1:length(left_wall)
+        x_source = left_wall(index, 1);
+        y_source = left_wall(index, 2);
+        distToMid = sqrt((x_source - 0.5).^2 + (y_source + 3.37).^2);
+        new_source = -1 * make_sink(x_source, y_source, x_space, y_space) * distToMid/(length(left_wall));
+        z_field = z_field + new_source;
+    end
+    
+    %right wall
+    for index = 1:length(right_wall)
+        x_source = right_wall(index, 1);
+        y_source = right_wall(index, 2);
+        distToMid = sqrt((x_source - 0.5).^2 + (y_source + ).^2);
+        new_source = -1 * make_sink(x_source, y_source, x_space, y_space) * distToMid/(length(right_wall));
+        z_field = z_field + new_source;
+    end
     
     %%%%%%%%%%%%%%%%%%%%
     % Display and return
     %%%%%%%%%%%%%%%%%%%%
     
-    contour(x_space,y_space,z_field)
+    s = surf(x_space,y_space,z_field, 'FaceAlpha', 0.5);
+    s.EdgeColor = 'none';
     
     res = z_field; %return
 end
