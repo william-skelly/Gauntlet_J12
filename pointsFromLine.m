7353f19c085380efcbcbdac52910452c8c6f0432
wskelly <wskelly@DMBQ2F3.olin.edu>
2022-05-09 10:18:19 -0400

    Now returns points for a line

--- pointsFromLine.m
+++ pointsFromLine.m
@@ -11,13 +11,13 @@ function res = pointsFromLine(slope_vector, start_point, end_point)
     % initialize paramers
     u_step    = 0.01; %the step size for the parameter u
     u_initial = 0; %the line is parametrized such that the start is at u=0
-    u_final   = ((end_point(1) - start_point(1)) / slope_vector(1); %the 
+    u_final   = (end_point(1) - start_point(1)) / slope_vector(1); %the 
         %value of the parameter that corresponds to the end of the line
     u_sweep   = u_initial:u_step:u_final; %note that because slope_vector 
         %is a unit vector, the parameter u is in meters
     
-    res = u_sweep;   
-         
-    %r(u) = [start_point(1) + slope_vector(1) * u, start_point(2) + slope_vector(2) * u];
-    
+    %calculate line
+    x_line = u_sweep .* slope_vector(1) + start_point(1);
+    y_line = u_sweep .* slope_vector(2) + start_point(2);
+    res    = [x_line', y_line']; %return
 end
