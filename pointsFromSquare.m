19b9c447a8d58ad7a94e94b7490a87edd8b7b42d
wskelly <wskelly@DMBQ2F3.olin.edu>
2022-05-09 17:36:57 -0400

    Now works for squares not at (0,0)

--- pointsFromSquare.m
+++ pointsFromSquare.m
@@ -9,8 +9,8 @@
 function res = pointsFromSquare(theta, side_length, center_point)
     %create central horizontal side_length line segment to use as archtype
     %for all sides
-    start_point = center_point - [side_length / 2; 0];
-    end_point   = center_point + [side_length / 2; 0];
+    start_point = [0,0] - [side_length / 2; 0];
+    end_point   = [0,0] + [side_length / 2; 0];
     archLine    = pointsFromLine([1,0], start_point, end_point);
     archLine    = [archLine,ones(length(archLine), 1)]'; %add column of 
                                                     %ones and transpose
@@ -21,6 +21,7 @@ function res = pointsFromSquare(theta, side_length, center_point)
     transform3 = [1,0,0; 0,1,side_length/2; 0,0,1];
     transform4 = [1,0,0; 0,1,-side_length/2; 0,0,1];
     rotate_theta = [cos(theta), -sin(theta), 0; sin(theta), cos(theta), 0; 0, 0, 1];
+    transformCenter = [1,0,center_point(1); 0,1,center_point(2); 0,0,1];
     
     %transform archside to create sides 1,2,3,4
     line1 = rotate_theta * transform1 * archLine;
@@ -30,6 +31,7 @@ function res = pointsFromSquare(theta, side_length, center_point)
     
     %stack all lines into an nx3 matrix
     all_lines = [line1, line2, line3, line4];
+    all_lines = transformCenter * all_lines; %translate to new center
     all_lines = all_lines(1:2,:); %crop off the Z=1 entries
     all_lines = all_lines'; %transpose into column
     
