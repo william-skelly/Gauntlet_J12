545ead593085661de57088e5a696f968610ba911
wskelly <wskelly@DMBQ2F3.olin.edu>
2022-05-09 17:58:27 -0400

    now has box sources

--- getPotField.m
+++ getPotField.m
@@ -28,6 +28,9 @@ function res = getPotField()
     left_wall     = pointsFromLine([1;0], [-1.5,-3.37], [2.5,-3.37]);
     right_wall    = pointsFromLine([1;0], [-1.5,1], [2.5,1]);
     corner_points = [-1.5, -3.37; -1.5, 1; 2.5, -3.37; 2.5, 1];
+    box1_points   = pointsFromSquare(pi/4, 0.5, [0.25; -1]);
+    box2_points   = pointsFromSquare(pi/4, 0.5, [1; -0.7]);
+    box3_points   = pointsFromSquare(0, 0.5, [1.41; -2]);
     
     %%%%%%%%%%%%%%%%
     % Create Sources
@@ -71,13 +74,53 @@ function res = getPotField()
         z_field = z_field + new_source;
     end
     
-    %add to corners
+    %box1
+    for index = 1:length(box1_points)
+        x_source = box1_points(index, 1);
+        y_source = box1_points(index, 2);
+        %distToMid = sqrt((x_source - 0.5).^2 + (y_source - 1).^2);
+        new_source = -1 * make_sink(x_source, y_source, x_space, y_space) * 0.5/(length(box1_points));
+        z_field = z_field + new_source;
+    end
+    
+    %box2
+    for index = 1:length(box2_points)
+        x_source = box2_points(index, 1);
+        y_source = box2_points(index, 2);
+        %distToMid = sqrt((x_source - 0.5).^2 + (y_source - 1).^2);
+        new_source = -1 * make_sink(x_source, y_source, x_space, y_space) * 0.5/(length(box2_points));
+        z_field = z_field + new_source;
+    end
+    
+    %box3
+    for index = 1:length(box3_points)
+        x_source = box3_points(index, 1);
+        y_source = box3_points(index, 2);
+        %distToMid = sqrt((x_source - 0.5).^2 + (y_source - 1).^2);
+        new_source = -1 * make_sink(x_source, y_source, x_space, y_space) * 0.5/(length(box3_points));
+        z_field = z_field + new_source;
+    end
+    
+    %corners
     for index = 1:length(corner_points)
         x_source = corner_points(index, 1);
         y_source = corner_points(index, 2);
         new_source = -1 * make_sink(x_source, y_source, x_space, y_space) / 4;
         % magnitude of corners compensates for sinks created by lines
         z_field = z_field + new_source;
+    end
+    
+    %%%%%%%%%%%%%%%%%%
+    % Add Sink for BoB
+    %%%%%%%%%%%%%%%%%%
+    
+    BoB_points = pointsFromCircle([.75,-2.5], .25)
+    
+    for index = 1:length(BoB_points)
+        x_source = BoB_points(index, 1);
+        y_source = BoB_points(index, 2);
+        new_sink = make_sink(x_source, y_source, x_space, y_space);
+        z_field = z_field + new_sink;
     end
     
     %%%%%%%%%%%%%%%%%%%%
