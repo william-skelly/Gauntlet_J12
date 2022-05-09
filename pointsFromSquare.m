bb543accd162b94dc4b0ef967186f34cc6e55925
wskelly <wskelly@DMBQ2F3.olin.edu>
2022-05-09 17:20:29 -0400

    initial commit

---
+++ pointsFromSquare.m
@@ -0,0 +1,38 @@
+% Returns a set of points that discretly represent a square
+%
+% PARAMS: theta        = the angle at which the square is oriented
+%         side_length  = the length of the side of the square
+%         center_point = the point at the center of the square
+% RETURN: a 2xn matrix where each row is a point x,y on the edge of the
+%         square
+
+function res = pointsFromSquare(theta, side_length, center_point)
+    %create central horizontal side_length line segment to use as archtype
+    %for all sides
+    start_point = center_point - [side_length / 2; 0];
+    end_point   = center_point + [side_length / 2; 0];
+    archLine    = pointsFromLine([1,0], start_point, end_point);
+    archLine    = [archLine,ones(length(archLine), 1)]'; %add column of 
+                                                    %ones and transpose
+    
+    %create transformation matices for sides 1,2,3,4
+    transform1 = [1,0,side_length/2; 0,1,0; 0,0,1] * [0,-1,0;1,0,0;0,0,1];
+    transform2 = [1,0,-side_length/2; 0,1,0; 0,0,1] * [0,-1,0;1,0,0;0,0,1];
+    transform3 = [1,0,0; 0,1,side_length/2; 0,0,1];
+    transform4 = [1,0,0; 0,1,-side_length/2; 0,0,1];
+    rotate_theta = [cos(theta), -sin(theta), 0; sin(theta), cos(theta), 0; 0, 0, 1];
+    
+    %transform archside to create sides 1,2,3,4
+    line1 = rotate_theta * transform1 * archLine;
+    line2 = rotate_theta * transform2 * archLine;
+    line3 = rotate_theta * transform3 * archLine;
+    line4 = rotate_theta * transform4 * archLine;
+    
+    %stack all lines into an nx3 matrix
+    all_lines = [line1, line2, line3, line4];
+    all_lines = all_lines(1:2,:); %crop off the Z=1 entries
+    all_lines = all_lines'; %transpose into column
+    
+    %return
+    res = all_lines;
+end
